d47f34779a776d9f915cd298a1a317e12931be91
ZIAN786 <zakirzian41@gmail.com>
2022-11-13 18:40:50 +0600

    complete registration

--- src/pages/auth/RegisterComplete.js
+++ src/pages/auth/RegisterComplete.js
@@ -1,6 +1,6 @@
 
 import React, {useState, useEffect} from "react"
-//import {auth} from "../../firebase";
+import {auth} from "../../firebase";
 import {toast} from "react-toastify";
 
 
@@ -8,8 +8,12 @@ const RegisterComplete = ({history}) =>{
 const [email, setEmail] = useState("");
 const [password, setPassword] = useState("");
 
-useState(()=> {
+useEffect(()=> {
 setEmail(window.localStorage.getItem("emailForRegistration"));
+
+//console.log(window.location.href);
+//console.log(window.localStorage.getItem("emailForRegistration"));
+
 }, []);
 
 
@@ -17,6 +21,44 @@ setEmail(window.localStorage.getItem("emailForRegistration"));
 
 const handleSubmit = async(e) =>{
  e.preventDefault();
+ //validation
+ if(!email || !password){
+    toast.error("Email and Password is required");
+    return;
+ }
+
+ if(password.length<6){
+    toast.error("Password must be 6 character long");
+    return;
+ }
+
+try{
+const result = await auth.signInWithEmailLink(
+    email,
+    window.location.href
+     );
+//console.log("Result", result);
+if(result.user.emailVerified){
+    
+    //remove user email from local storage
+     window.localStorage.removeItem("emailForRegistration");
+
+    //get user id token
+    let user = auth.currentUser
+    await user.updatePassword(password);
+    const idTokenResult = await user.getIdTokenResult()
+
+    //redux store
+    console.log("user", user, "idTokenResult", idTokenResult);
+
+    //redirect
+    //history.push("/")
+}
+
+}catch(error){
+    console.log(error);
+    toast.error(error.message);
+}
 
 };
 
